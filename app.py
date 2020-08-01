8f0ceb47d78ce6bd9c9ec23000b2f4d5d18f941f
RuslanPr0g <59767834+RuslanPr0g@users.noreply.github.com>
2020-08-01 10:09:49 +0300

    fix cross_allowed_origins

--- app.py
+++ app.py
@@ -1,16 +1,9 @@
 from flask import Flask
-from flask_socketio import SocketIO, send
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'fk;'  # it needs to get rid of that in the future
-socketio = SocketIO(app)
-
-
-@socketio.on('message')
-def handleMessage(msg):
-    print('14:02 - ' + msg)
-    send(msg, broadcast=True)
-
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 if __name__ == '__main__':
     socketio.run(app)
