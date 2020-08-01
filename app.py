9fb31a2a27a7199fdf3081f202ae8fae29131285
RuslanPr0g <59767834+RuslanPr0g@users.noreply.github.com>
2020-08-01 09:41:49 +0300

    flask raedy

--- app.py
+++ app.py
@@ -2,8 +2,15 @@ from flask import Flask
 from flask_socketio import SocketIO, send
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'fk;'
+app.config['SECRET_KEY'] = 'fk;'  # it needs to get rid of that in the future
 socketio = SocketIO(app)
+
+
+@socketio.on('message')
+def handleMessage(msg):
+    print('14:02 - ' + msg)
+    send(msg, broadcast=True)
+
 
 if __name__ == '__main__':
     socketio.run(app)
