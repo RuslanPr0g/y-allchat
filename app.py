418a518b7aab7fe9bb36ffccc001802890751849
RuslanPr0g <59767834+RuslanPr0g@users.noreply.github.com>
2020-08-01 10:19:19 +0300

    fix def

--- app.py
+++ app.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, cors_allowed_origins="*")
 
+@socketio.on('message')
+def handleMessage(msg):
+	send(msg, broadcast=True)
+
 if __name__ == '__main__':
     socketio.run(app)
